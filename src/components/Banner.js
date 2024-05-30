7908518926990265e3f966a620d2a20ecba48903
jzho954 <jzho954@aucklanduni.ac.nz>
2024-05-30 18:45:27 +1200

    a

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -78,11 +78,7 @@ export const Banner = () => {
                     </span>
                   </h1>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
+                    Recent graduate from the University Of Auckland. Eager to put my skills to use from universtiy and my personal projects. I am a passionate developer who loves to create and design websites. I am always looking for new opportunities to learn and grow.
                   </p>
 
                   <a
